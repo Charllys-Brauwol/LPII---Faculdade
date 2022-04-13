995f017e7a19bcacc61422f0f4f9da56285ac6e3
Charllys Brauwol <charllysbrauwolhaha@gmail.com>
2022-04-13 18:35:03 -0300

    Questão 01 - OK

--- Atividade 01/01 - Maior Numero/MaiorNumero/src/maiornumero/MaiorNumero.java
+++ Atividade 01/01 - Maior Numero/MaiorNumero/src/maiornumero/MaiorNumero.java
@@ -1,7 +1,7 @@
 /*
 Crie uma classe java MaiorNumero que contenha um método que receba dois
 números inteiros e imprima o maior entre eles.
-*/
+ */
 package maiornumero;
 
 import java.util.Scanner;
@@ -10,27 +10,26 @@ import java.util.Scanner;
  * @author Charllys Brauwol
  */
 public class MaiorNumero {
-    
+
     public static void main(String[] args) {
         //Declaração de Váriavéis
         int numero1;
         int numero2;
-        
+
         //Metódos de Entrada de Dados
         Scanner digitar = new Scanner(System.in);
         System.out.println("Digite o Primeiro Numero:");
         numero1 = digitar.nextInt();
-        
+
         System.out.println("Digite o Segundo Numero:");
         numero2 = digitar.nextInt();
-        
-        
+
         //Estrutura de Decisão para comparar os dois números
-        if(numero1 > numero2){
-        System.out.println("O Primeiro Numero digitado e maior:" + numero1);
-        }else if (numero2 > numero1){
+        if (numero1 > numero2) {
+            System.out.println("O Primeiro Numero digitado e maior:" + numero1);
+        } else if (numero2 > numero1) {
             System.out.println("O Segundo e maior:" + numero2);
-        }else {
+        } else {
             System.out.println("Os dois sao Iguais");
         }
     }
