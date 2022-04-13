876bf92ce6a6346d29e828785a4e361ee9b9009e
Charllys Brauwol <charllysbrauwolhaha@gmail.com>
2022-04-13 18:20:10 -0300

    Questão 2

---
+++ Atividade 01/02 - Decrescente/NumeroDecrescente/src/numerodecrescente/NumeroDecrescente.java
@@ -0,0 +1,17 @@
+/**
+Crie uma classe java NumeroDecrescente que contenha um método que receba um
+número inteiro e imprima, em ordem decrescente, o valor do número até 0
+ */
+package numerodecrescente;
+
+/**
+ *
+ * @author Charllys Brauwol
+ */
+public class NumeroDecrescente {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
