8f32a2fa9f7415c2a28f46144405092e98c67f69
Charllys Brauwol <charllysbrauwolhaha@gmail.com>
2022-04-14 09:14:03 -0300

    Questão 03

---
+++ Atividade 01/03 - Impares/NumerosImpares/src/numerosimpares/NumerosImpares.java
@@ -0,0 +1,18 @@
+/*
+Escreva um programa que imprima na tela a soma dos números ímpares entre 0 e 30 e
+a multiplicação dos números pares entre 0 e 30, começando do 2.
+ */
+package numerosimpares;
+
+/**
+ *
+ * @author Charllys Brauwol
+ */
+public class NumerosImpares {
+
+    public static void main(String[] args) {
+        //Declaração de Váriavéis
+      
+    }
+
+}
