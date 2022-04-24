7245f30b76d4b3e672c63e2baca45acd0957793c
Charllys Brauwol <charllysbrauwolhaha@gmail.com>
2022-04-24 15:32:33 -0300

    Questão 04 - Veiculos

---
+++ Trabalho POO - Java 01/ProjetoVeiculos/src/projetoveiculos/Veiculos.java
@@ -0,0 +1,35 @@
+
+package projetoveiculos;
+
+/**
+ *
+ * @author Charllys e Jonas
+ */
+public class Veiculos {
+    String placa;
+    int ano;
+
+    public Veiculos(String placa, int ano) {
+        this.placa = placa;
+        this.ano = ano;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+    
+    public void exibirDados(){};
+    }
+    
