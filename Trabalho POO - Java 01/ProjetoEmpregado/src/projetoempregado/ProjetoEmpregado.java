4094a9fba84af945505cda48f641b9385cce66f3
Charllys Brauwol <charllysbrauwolhaha@gmail.com>
2022-05-06 11:13:45 -0300

    Projeto Empregado

--- Trabalho POO - Java 01/ProjetoEmpregado/src/projetoempregado/ProjetoEmpregado.java
+++ Trabalho POO - Java 01/ProjetoEmpregado/src/projetoempregado/ProjetoEmpregado.java
@@ -8,7 +8,6 @@ public class ProjetoEmpregado {
 
     public static void main(String[] args) {
         Assalariado a = new Assalariado(1400, "Joao ", "Da Silva", "123");
-        System.out.println(a.getNome() + a.getSobrenome() + " RECEBE UM SALARIO DE " + a.vencimento() + " E POSSUI O CPF" + a.getCpf());
         System.out.println("====================");
         
         Comissionado b = new Comissionado(1500, 5, "Joao", "Da Silva", "123");
